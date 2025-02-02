48f508f66fd16a32cfafba593ea1667593c6cb6d
PrAsAnNaRePo <prasannatwenty@gmail.com>
2025-02-02 22:55:56 +0530

    added arguments control for test script

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
+import argparse
 
-base_model = "Qwen/Qwen2.5-7B-Instruct"
-adapter_model = "nnpy/qwen2-7b-chat-ft" # replace with your adapter model
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_id", type=str, default="Qwen/Qwen2.5-7B-Instruct", help="HF Model ID")
+parser.add_argument("--adapter_model", type=str, required=True, help="Path to the adapter model")
+parser.add_argument("--prompt", type=str, required=True, help="Prompt to use for generation")
+
+args = parser.parse_args()
+
+base_model = args.model_id
+adapter_model = args.adapter_model
+prompt = args.prompt
 
 model = AutoModelForCausalLM.from_pretrained(base_model, device_map='auto')
 model = PeftModel.from_pretrained(model, adapter_model)
@@ -10,13 +20,11 @@ tokenizer = AutoTokenizer.from_pretrained(base_model)
 
 model.eval()
 
-prompt = "hey whatup!"
-
 tokenized = tokenizer.apply_chat_template(
     [
         {
             'role': 'user',
-            'content': 'hey love you babe'
+            'content': prompt
         }
     ],
     tokenize=False
@@ -24,5 +32,5 @@ tokenized = tokenizer.apply_chat_template(
 
 tokens = tokenizer(tokenized, return_tensors='pt').to('cuda')
 
-res = model.generate(**tokens, max_new_tokens=100)
+res = model.generate(**tokens, max_new_tokens=150)
 print(tokenizer.decode(res.reshape(-1)))
